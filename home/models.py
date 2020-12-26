ac458e17a9f9ae4ef4a8b42b5af81f2924f736e6
aamit2267 <aamit2267@gmail.com>
2020-12-26 13:58:41 +0530

    Edited templates
    added contact.html inside home of templates

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
+    content = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
